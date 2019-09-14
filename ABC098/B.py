790d1799ce3d27e59ad6a32e92a05791cd293474
shiki7 <bitbite8@gmail.com>
2019-09-14 21:07:29 +0900

    add

--- ABC098/B.py
+++ ABC098/B.py
@@ -1,2 +1,7 @@
 N = int(input())
 S = input()
+s_list = [s for s in S]
+max_count = 0
+for i in range(1, N):
+    max_count = max(max_count, len(set(s_list[:i]) & set(s_list[i:])))
+print(max_count)
