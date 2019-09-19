ebf9dd137181d94f402316292b48fb7ad0b30487
shiki7 <bitbite8@gmail.com>
2019-09-19 19:35:34 +0900

    Create B.py

---
+++ ABC081/B.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+ans = 0
+while True:
+    for i in range(n):
+        if a[i] % 2 == 1:
+            print(ans)
+            exit()
+        a[i] //= 2
+    ans += 1
