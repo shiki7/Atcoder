b5a63cbf3114b7dd915f9a2ec92023c0f870a382
shiki7 <bitbite8@gmail.com>
2019-09-22 22:15:46 +0900

    add

---
+++ ABC073/A.py
@@ -0,0 +1,6 @@
+n = input()
+for i in n:
+    if i == '9':
+        print('Yes')
+        exit()
+print('No')
---
+++ ABC073/B.py
@@ -0,0 +1,6 @@
+n = int(input())
+lr = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+for l, r in lr:
+    ans += r-l + 1
+print(ans)
