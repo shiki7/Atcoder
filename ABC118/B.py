418a9858fae6c6ea5a7125e7e847a43305836616
shiki7 <bitbite8@gmail.com>
2019-09-27 17:57:09 +0900

    Update B.py

--- ABC118/B.py
+++ ABC118/B.py
@@ -1,6 +1,16 @@
-n = int(input())
-s = list(input().split())
-if len(set(s)) == 3:
-    print('Three')
-else:
-    print('Four')
+n, m = map(int, input().split())
+ka = [list(map(int, input().split())) for _ in range(n)]
+
+if n == 1:
+    print(ka[0][0])
+    exit()
+
+counter = [0] * m
+for x in ka:
+    for a in x[1:]:
+        counter[a - 1] += 1
+ans = 0
+for i in range(m):
+    if counter[i] == n:
+        ans += 1
+print(ans)
