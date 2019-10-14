13e12e87443a12c7395a080f3a64833056c3973e
shiki7 <bitbite8@gmail.com>
2019-10-15 08:57:26 +0900

    Create C.py

---
+++ ABC053/C.py
@@ -0,0 +1,15 @@
+x = int(input())
+if x / 11 < 0:
+    if x <= 6:
+        print(1)
+    else:
+        print(2)
+else:
+    a = x // 11
+    b = x % 11
+    if b == 0:
+        print(2 * a)
+    elif b <= 6:
+        print(2 * a + 1)
+    else:
+        print(2 * a + 2)
