0cdb8cae9601105b656172550fe4a946f19f1a0a
shiki7 <bitbite8@gmail.com>
2019-10-23 09:00:36 +0900

    Update D.py

--- ABC143/D.py
+++ ABC143/D.py
@@ -1,5 +1,4 @@
-# TLE not clear
-import itertools
+import bisect
 
 n = int(input())
 L = list(map(int, input().split()))
@@ -7,11 +6,8 @@ L = list(map(int, input().split()))
 ans = 0
 L.sort()
 
-for i in range(n-2):
-    for j in range(i+1, n-1):
-        for k in range(j+1, n):
-            if L[k] < L[i]+L[j]:
-                ans += 1
-            else:
-                break
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        idx = bisect.bisect_left(L, L[i] + L[j])
+        ans += max(0, idx - j - 1)
 print(ans)
