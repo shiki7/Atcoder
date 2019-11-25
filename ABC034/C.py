ae581394976f31ea0085584b3e3fa7b0d5763e41
shiki7 <bitbite8@gmail.com>
2019-11-26 08:45:02 +0900

    Create C.py

---
+++ ABC034/C.py
@@ -0,0 +1,11 @@
+w, h = map(int, input().split())
+
+def nCr(n, r, mod=10**9+7):
+    r = min(r, n-r)
+    numer = denom = 1
+    for i in range(1, r+1):
+        numer = numer * (n+1-i) % mod
+        denom = denom * i % mod
+    return numer * pow(denom, mod-2, mod) % mod
+
+print(nCr(w+h-2, h-1))
