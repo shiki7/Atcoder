68b483da23490968d5b877a1975ed00799b037be
shiki7 <bitbite8@gmail.com>
2019-12-01 23:30:33 +0900

    add

--- SMTB/D.py
+++ SMTB/D.py
@@ -1,22 +1,22 @@
 n = int(input())
 s = input()
 
-# どこか間違ってる
+# TLE
 count = 0
-for a in range(9):
-    for b in range(9):
-        for c in range(9):
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
             flag = 0
             for i in range(0, n-2):
                 if flag == 1:
                     break
-                if int(s[i]) == b:
+                if int(s[i]) == c:
                     for j in range(i+1, n-1):
                         if flag == 1:
                             break
-                        if int(s[j]) == a:
+                        if int(s[j]) == b:
                             for k in range(j+1, n):
-                                if int(s[k]) == c:
+                                if int(s[k]) == a:
                                     count += 1
                                     flag = 1
                                     break
