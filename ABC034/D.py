3fa3581b2dfc91a02ba67758b6aafb179e135ac8
shiki7 <bitbite8@gmail.com>
2020-02-19 08:11:39 +0900

    Create D.py

---
+++ ABC034/D.py
@@ -0,0 +1,23 @@
+def check(mid, wp, k):
+    arr = []
+    for i in range(n):
+        w = wp[i][0]
+        p = wp[i][1]
+        arr.append(w * (p-mid))
+    return sum(sorted(arr, reverse=True)[:k]) > 0
+
+
+n, k = map(int, input().split())
+wp = [list(map(int, input().split())) for _ in range(n)]
+left = 0
+right = 100
+
+for i in range(1000):
+    mid = (left+right)/2
+    if check(mid, wp, k):
+        left = mid
+        ans = mid
+    else:
+        right = mid
+        ans = mid
+print(ans)
