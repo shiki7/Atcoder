9fa59e61117be49e14f45f5d4ae4c78aaf1f6466
shiki7 <bitbite8@gmail.com>
2020-02-25 19:28:00 +0900

    Update D.py

--- ABC147/D.py
+++ ABC147/D.py
@@ -1,26 +1,17 @@
-import itertools
-
-
-def xORSum(arr, n):
-    ans = 0
-    for i in range(0, n):
-        zc = 0
-        oc = 0
-        idsum = 0
-        for j in range(i, n):
-            if (arr[j] % 2 == 0):
-                zc = zc + 1
-
-            else:
-                oc = oc + 1
-            arr[j] = int(arr[j] / 2)
-
-        idsum = oc * zc * (1 << i)
-        ans = ans + idsum
-    return ans
-
+import numpy as np
 
 n = int(input())
-a = list(map(int, input().split()))
+a = np.array(list(map(int, input().split())))
+mod = 10**9+7
+ 
+ans = 0
+max_digit = 0
+for i in range(n):
+    max_digit = max(max_digit, len(bin(a[i])))
 
-print(xORSum(a, n) % (10**9+7))
+# 各桁の2**i * （0の数 * 1の数） の和
+for i in range(max_digit):
+    num1 = np.count_nonzero((a >> i) & 1)  # 1の数
+    num2 = n - num1  # 0の数
+    ans += pow(2, i, mod) * (num1 * num2) % mod
+print(ans % mod)
