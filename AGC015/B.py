20a664e798335e27287d1066a6b3bfae35ca79d3
shiki7 <bitbite8@gmail.com>
2020-02-26 19:48:26 +0900

    Update B.py

--- AGC015/B.py
+++ AGC015/B.py
@@ -1,17 +1,9 @@
-n = int(input())
-hl = [list(map(float, input().split())) for _ in range(n)]
-count = [0 for _ in range(6)]
-for high, low in hl:
-    if high >= 35:
-        count[0] += 1
-    if 30 <= high < 35:
-        count[1] += 1
-    if 25 <= high < 30:
-        count[2] += 1
-    if low >= 25:
-        count[3] += 1
-    if low < 0 and high >= 0:
-        count[4] += 1
-    if high < 0:
-        count[5] += 1
-print(' '.join(map(str, count)))
+s = input()
+n = len(s)
+ans = 0
+for i in range(n):
+    if s[i] == 'U':
+        ans += n-i-1 + 2*i
+    else:
+        ans += 2*(n-i-1) + i
+print(ans)
