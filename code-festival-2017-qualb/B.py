d882bfa59c6e089aec7cff8de5e85010133a7d89
shiki7 <bitbite8@gmail.com>
2020-03-02 08:12:22 +0900

    Create B.py

---
+++ code-festival-2017-qualb/B.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+n = int(input())
+d = list(map(int, input().split()))
+m = int(input())
+t = list(map(int, input().split()))
+
+counter1 = Counter(d)
+counter2 = Counter(t)
+
+for k, v in counter2.items():
+    if counter1[k] < v:
+        print('NO')
+        exit()
+print('YES')
