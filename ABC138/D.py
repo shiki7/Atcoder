62dc0bd6eb8d65cf63bb3e8ea7826baa372e2dce
shiki7 <bitbite8@gmail.com>
2020-03-04 09:57:46 +0900

    Update D.py

--- ABC138/D.py
+++ ABC138/D.py
@@ -1,10 +1,33 @@
-# -*- coding: utf-8 -*-
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+ 
+ 
+def dfs(cur, par):
+    for child in edge[cur]:
+        if child == par:
+            continue
+        counter[child] += counter[cur]
+        dfs(child, cur)
+ 
+ 
 N, Q = map(int, input().split())
-a = []
-b = []
-p = []
-x = []
-for i in range(N):
-    a[i], b[i] = map(int, input().split())
+ab = [list(map(int, input().split())) for _ in range(N-1)]
+px = [list(map(int, input().split())) for _ in range(Q)]
+counter = [0] * N
+ 
+edge = [[] for _ in range(N)]
+for i in range(N-1):
+    a = ab[i][0] - 1
+    b = ab[i][1] - 1
+    edge[a].append(b)
+    edge[b].append(a)
+ 
 for i in range(Q):
-    p[i], x[i] = map(int, input().split())
+    p = px[i][0] - 1
+    x = px[i][1]
+    counter[p] += x
+ 
+ 
+dfs(0, -1)
+print(*counter)
