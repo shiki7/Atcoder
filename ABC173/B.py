f08bf66aad75c954758d8e73b5dcb2fcabaafd51
shiki <bitbite8@gmail.com>
2020-07-08 17:34:47 +0900

    Create B.py

---
+++ ABC173/B.py
@@ -0,0 +1,6 @@
+N = int(input())
+S = [input() for _ in range(N)]
+print("AC x " + str(S.count("AC")))
+print("WA x " + str(S.count("WA")))
+print("TLE x " + str(S.count("TLE")))
+print("RE x " + str(S.count("RE")))
