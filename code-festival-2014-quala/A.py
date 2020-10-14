2762b9da6540c6ecbb7c1562d915fb261c9517e3
shiki7 <bitbite8@gmail.com>
2020-10-15 07:02:48 +0900

    add

--- code-festival-2014-quala/A.py
+++ code-festival-2014-quala/A.py
@@ -1,8 +1,2 @@
-n = int(input())
-for i in range(10**5):
-    if i**3 == n:
-        print('YES')
-        exit()
-    if i**3 > n:
-        print('NO')
-        exit()
+S = input()
+print(S + "2014")
