7aa30f3ef75dffba55517c88afa43b2fdb3d388a
shiki7 <bitbite8@gmail.com>
2020-10-25 07:45:39 +0900

    add

---
+++ ABC180/C.py
@@ -0,0 +1,15 @@
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+    return divisors
+
+
+N = int(input())
+ans = make_divisors(N)
+ans = sorted(ans)
+for i in range(len(ans)):
+    print(ans[i])
