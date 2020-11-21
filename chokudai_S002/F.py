127e85f0ec197601a167ec20d1e15021963b0e8b
shiki7 <bitbite8@gmail.com>
2020-11-21 09:40:35 +0900

    add

--- chokudai_S002/F.py
+++ chokudai_S002/F.py
@@ -1,7 +1,7 @@
 N = int(input())
 ab = [list(map(int, input().split())) for _ in range(N)]
-li = set()
-for i in range(len(ab)):
-    print(sorted(ab[i]))
-    li += [sorted(ab[i])]
-print(li)
+# li = set()
+# for i in range(len(ab)):
+#     print(sorted(ab[i]))
+#     li += [sorted(ab[i])]
+# print(li)
