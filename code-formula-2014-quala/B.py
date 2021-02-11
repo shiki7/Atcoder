763d99a6025af0cbfea41dbff86ec8df9ec019e2
shiki7 <bitbite8@gmail.com>
2021-02-11 19:55:59 +0900

    add

---
+++ code-formula-2014-quala/B.py
@@ -0,0 +1,16 @@
+_ = input()
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+p = ['.' if v in A else ('o' if v in B else 'x') for v in range(10)]
+for i in range(10):
+    if i in A:
+        p.append('.')
+    elif i in B:
+        p.append('o')
+    else:
+        p.append('x')
+
+print('{0} {1} {2} {3}'.format(p[7], p[8], p[9], p[0]))
+print(' {0} {1} {2}'.format(p[4], p[5], p[6]))
+print('  {0} {1}'.format(p[2], p[3]))
+print('   {0}'.format(p[1]))
