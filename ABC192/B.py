37033a852791b9e9a938b3bebfd32106f6ae944e
shiki7 <bitbite8@gmail.com>
2021-02-28 09:17:39 +0900

    add

---
+++ ABC192/B.py
@@ -0,0 +1,11 @@
+s = input()
+for i in range(len(s)):
+    if i % 2 == 0:
+        if s[i].isupper():
+            print('No')
+            exit()
+    else:
+        if s[i].islower():
+            print('No')
+            exit()
+print('Yes')
