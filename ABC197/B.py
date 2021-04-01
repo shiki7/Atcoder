4574e42106f54635ffcac6f65186f1386db0219d
shiki7 <bitbite8@gmail.com>
2021-04-01 15:37:46 +0900

    add

---
+++ ABC197/B.py
@@ -0,0 +1,22 @@
+h, w, x, y = map(int, input().split())
+ans = 1
+s = [list(input()) for _ in range(h)]
+x -= 1
+y -= 1
+for i in range(x+1, h):
+    if s[i][y] == "#":
+        break
+    ans += 1
+for i in range(x-1, -1, -1):
+    if s[i][y] == "#":
+        break
+    ans += 1
+for i in range(y-1, -1, -1):
+    if s[x][i] == "#":
+        break
+    ans += 1
+for i in range(y+1, w):
+    if s[x][i] == "#":
+        break
+    ans += 1
+print(ans)
