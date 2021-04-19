87c6131c0c6a4ac37dac841f69884fd17a9e4057
shiki7 <bitbite8@gmail.com>
2021-04-20 00:05:20 +0900

    add

--- jsc2021/C.py
+++ jsc2021/C.py
@@ -1,14 +1,5 @@
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-    while a % b != 0:
-        a, b = b, a % b
-    return b
-
-
-ans = 1
 a, b = map(int, input().split())
-for i in range(a, b):
-    for j in range(i+1, b+1):
-        ans = max(ans, gcd(i, j))
-print(ans)
+for c in range(b, 0, -1):
+    if (a + c - 1) // c < b // c:
+        print(c)
+        exit()
