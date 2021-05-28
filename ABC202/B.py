9953dc9288beb7a6b8ab47b447af032a205ff37e
shiki7 <bitbite8@gmail.com>
2021-05-28 21:52:55 +0900

    add

---
+++ ABC202/B.py
@@ -0,0 +1,11 @@
+S = input()
+S = S[::-1]
+ans = ""
+for i in range(len(S)):
+    if S[i] == '6':
+        ans += '9'
+    elif S[i] == '9':
+        ans += '6'
+    else:
+        ans += S[i]
+print(ans)
