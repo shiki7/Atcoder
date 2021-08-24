8e8871ff22d5804e3ab85721977b914489ea64fa
shiki7 <bitbite8@gmail.com>
2021-08-24 15:21:24 +0900

    add

---
+++ ABC210/B.py
@@ -0,0 +1,9 @@
+N = int(input())
+S = input()
+for i in range(N):
+    if S[i] == '1':
+        if i % 2 == 0:
+            print('Takahashi')
+        else:
+            print('Aoki')
+        exit()
